ea022833c455650f2fe913468196782b5ec0900e
Harshad Tembhe <Harshadtembhe1234@gmail.com>
2024-04-30 14:27:09 +0530

    Calculate Employee Wage for Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,25 +3,24 @@ public class EmployeeWage {
     public static void main(String[] args){
         System.out.print("Welcome to Employee Wage Computation: ");
         Random ran = new Random();
-        int attend = ran.nextInt(3);
-        int employee_rate_hr = 20, work_hr=0;
-        System.out.println(attend);
-        switch (attend) {
-            case 0:
-                System.out.println("Employee Absent");
-                break;
-            case 1:
-                work_hr = 4;
-                break;
-            case 2:
-                work_hr =8;
-                break;
-            default:
-                break;
+        int employee_rate_hr = 20, work_hr=0, sal=0;
+        for (int i=0; i<=20; i++){
+            int attend = ran.nextInt(3);
+            switch (attend) {
+                case 1:
+                    work_hr = 4;
+                    break;
+                case 2:
+                    work_hr =8;
+                    break;
+                default:
+                    break;
+            }
+            if (work_hr!=0){
+                int x = work_hr*employee_rate_hr;
+                sal+=x;
+            }
         }
-        if (work_hr!=0){
-            int x = work_hr*employee_rate_hr;
-            System.out.println("Employee Present and it's wage is "+x);
-        }
+        System.out.println("Employee Present and it's wage is "+sal);
     }
 }
