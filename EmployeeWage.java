05121623073c874c5244c0f7c65c0bdf3a914dec
Harshad Tembhe <Harshadtembhe1234@gmail.com>
2024-04-30 13:57:46 +0530

    Part time and full time Employ wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,13 +4,21 @@ public class EmployeeWage {
         System.out.print("Welcome to Employee Wage Computation");
         Random ran = new Random();
         boolean attend = ran.nextBoolean();
-        int employee_rate_hr = 20, work_hr=8;
+        int employee_rate_hr = 20, work_hr;
         System.out.println(attend);
         if (attend==true){
+            int attend_hr = ran.nextInt(2);
+            if (attend_hr==0){
+                work_hr = 4;
+            }
+            else{
+                work_hr = 8;
+            }
             int x = work_hr*employee_rate_hr;
             System.out.println("Employee Present and it's wage is "+x);
-        }else{
-            System.out.println("Absent");
+        }
+        else{
+            System.out.println("Employee Absent");
         }
     }
 }
