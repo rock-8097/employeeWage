d23716b5685e0d35961e09bf1b784226307776bc
Harshad Tembhe <Harshadtembhe1234@gmail.com>
2024-05-21 09:11:10 +0530

    Employee wage for multiple companies

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ public class Main {
         Company company1 = new Company("Company A", 20);
         Company company2 = new Company("Company B", 25);
 
-        Employee emp1 = new Employee(company1);
-        Employee emp2 = new Employee(company2);
+        EmpWageBuilder emp1 = new EmpWageBuilder(company1);
+        EmpWageBuilder emp2 = new EmpWageBuilder(company2);
 
-        int workCompany1 = emp1.calculateWageMonth();
-        int workCompany2 = emp2.calculateWageMonth();
+        int workCompany1 = emp1.computeWage();
+        int workCompany2 = emp2.computeWage();
 
         System.out.println("Total wage for " + company1.getName() + ": " + workCompany1);
         System.out.println("Total wage for " + company2.getName() + ": " + workCompany2);
