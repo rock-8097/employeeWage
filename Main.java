e8c10d1dd6295c87e03ee68194549074163c291f
Harshad Tembhe <Harshadtembhe1234@gmail.com>
2024-05-21 15:05:07 +0530

    Refactor to have list of multiple companies to manage Employee Wage.

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //How many company store in array
-        CompanyEmpWage companyEmpWage = new CompanyEmpWage(2);
+        CompanyEmpWage companyEmpWage = new CompanyEmpWage();
         //company Employee Wage Set here
         companyEmpWage.addCompany("Company A", 500, 8, 4);
         companyEmpWage.addCompany("Company B", 700, 10, 5);
